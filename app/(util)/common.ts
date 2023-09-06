7ac8c722d0ddd2fd2c249f137594b3241701fbc3
TUYIZERE Samuel <samueltuyizere0@gmail.com>
2023-09-06 12:05:57 +0200

    added common assets

---
+++ app/(util)/common.ts
@@ -0,0 +1,5 @@
+import logo from '../(assets)/images/timbuktu.png'
+import group from '../(assets)/images/group.png'
+
+export const timbuktuLogo = logo.src
+export const groupOfMillenials = group.src
